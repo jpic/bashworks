f7c536a0ea0061b60b005bd21930dff9194520d2
james pic <jamespic@gmail.com>
2009-09-29 18:19:11 +0200

    Port of the 'break' module

--- break/conf.sh
+++ break/conf.sh
@@ -6,23 +6,30 @@
 ##	@License	Apache
 #--------------------------
 
+unset break_variables
+break_variables+=("interval")
+break_variables+=("previous")
+# prefix variable names
+break_variables=("${break_variables[@]/#/break_}")
+
 #--------------------------
 ## Save your break configuration
 #--------------------------
 function break_conf_save() {
-    conf_save break
+    conf_save $break_conf_path $break_variables
 }
 
 #--------------------------
 ## Load your break configuration
 #--------------------------
 function break_conf_load() {
-    conf_load break
+    conf_save $break_conf_path
 }
 
 #--------------------------
 ## Interactive module configuration
 #--------------------------
 function break_conf_interactive() {
-    conf_interactive break
+    conf_interactive $break_variables
+    break_conf_save
 }
--- break/source.sh
+++ break/source.sh
@@ -4,38 +4,42 @@
 ##	@Synopsis	Break management module
 ##	@Copyright	Copyright 2009, James Pic
 ##	@License	Apache
-## This module is ideal when you can't manage your hackers: let the GOD BASH
-## do it.
+##
+## This "lifestyle" module helps bash users to manage their breaks times.
+##
+## It is particularely good for your health if you use to drink coffee or
+## smoke during your breaks.
+##
+## The minimal interval between breaks should be configured and saved
+## before this module can do any good.
+##
+## Then it is possible to use break_request() to ask for permission to
+## take a break.
+##
+## In case of rebellion then break_do() should be directly used.
 #--------------------------
 
 #--------------------------
-## Declares module configuration variable names.
+## Module source callback
+##
+## This function should be called when the modhule is loaded. It will
+## load the conf and functions submodules.
 #--------------------------
 function break_source() {
-    unset break_variables
-    break_variables+=("interval")
-    break_variables+=("previous")
-    # prefix variable names
-    break_variables=("${break_variables[@]/#/break_}")
-
-
-    jpic_module_source break functions.sh
-    jpic_module_source break conf.sh
-
-    break_defaults_setter
+    source $(module_get_path break)/functions.sh
+    source $(module_get_path break)/conf.sh
 }
 
 #--------------------------
-## Sets the default break interval to 7200 and conf path to ~/.break
-#--------------------------
-function break_defaults_setter() {
-    break_interval=7200
-    break_conf_path=${HOME}/.break
-}
-
-#--------------------------
-## Load configuration on init
+## Module initialization callback
+##
+## This function is responsible for putting the module in a useable state.
+##
+## It tryes to set some defaults and load the user configuration data.
 #--------------------------
 function break_init() {
+    break_interval=7200
+    break_conf_path=${HOME}/.break
+
     break_conf_load
 }
