fcb369d442970b1bd2c4ec5a9c68dc9fcc9f50e8
jpic <jamespic@gmail.com>
2009-09-29 03:03:07 +0200

    Changed all 'config' to 'conf' in names, and slightly refactored the module conf path finder

--- conf/source.sh
+++ conf/source.sh
@@ -40,24 +40,24 @@ function conf_reset() {
 ## <p>Saves variables of a module in a file in a loadable format.</p>
 ## <p>
 ## If the module configuration path param was not specified then it will try to
-## to use $yourmodule_config_path <b>after</b> trying to call
-## yourmodule_config_path_setter(), which could set $yourmodule_config_path.
+## to use $yourmodule_conf_path <b>after</b> trying to call
+## yourmodule_conf_path_setter(), which could set $yourmodule_conf_path.
 ## </p><p>
 ## Note that it will automatically try to create missing directories and inform
 ## the user.
 ## </p>
 ## @param Module name
 ## @param Module configuration path (optionnal)
-## @return 2 If it could not save the module variables into a config file.
+## @return 2 If it could not save the module variables into a conf file.
 #-------------------------- 
 function conf_save() {
-    local usage="conf_save \$module_name [\$module_config_path]"
+    local usage="conf_save \$module_name [\$module_conf_path]"
     local module_name="$1"
-    local module_config_path="$2"
-    # config path variable callback
-    local module_config_path_setter="${module_name}_set_config_path"
-    # config path variable
-    local module_config_path_varname="${module_name}_config_path"
+    local module_conf_path="$2"
+    # conf path variable callback
+    local module_conf_path_setter="${module_name}_conf_path_setter"
+    # conf path variable
+    local module_conf_path_varname="${module_name}_conf_path"
     # module variables array
     local module_variables="${module_name}_variables[@]"
 
@@ -67,49 +67,47 @@ function conf_save() {
     fi
 
     # if $2 was not specified
-    # and ${module_name}_set_config_path is a function
-    if [[ -z $module_config_path ]]; then
-        if [[ $(declare -f $module_config_path_setter) ]]; then
+    # and ${module_name}_set_conf_path is a function
+    if [[ -z $module_conf_path ]]; then
+        if [[ $(declare -f $module_conf_path_setter) ]]; then
             # run the function
-            $module_config_path_setter
+            $module_conf_path_setter
 
-            if [[ -z $module_config_path ]]; then
-                jpic_print_error "Dying: $module_config_path_setter() did not set \$$module_config_path"
+            if [[ -z ${!module_conf_path_varname} ]]; then
+                jpic_print_error "Dying: $module_conf_path_setter() did not set \$$module_conf_path"
                 jpic_print_error "Usage: $usage"
                 return 2
             fi
-        # and ${module_name}_config_path is not a non-zero variable
-        elif [[ -n ${!module_config_path_varname} ]]; then
-            # use that variable
-            module_config_path="${!module_config_path_varname}"
-        else
-            jpic_print_error "Dying: could not figure ${module_name} config path"
+        elif [[ -z ${!module_conf_path_varname} ]]; then
+            jpic_print_error "Dying: could not figure ${module_name} conf path"
             jpic_print_error "Usage: $usage"
             return 2
         fi
     fi
+    
+    module_conf_path="${!module_conf_path_varname}"
 
-    if [[ ! -f $module_config_path ]]; then
-        local directory=${module_config_path%/*}
+    if [[ ! -f $module_conf_path ]]; then
+        local directory=${module_conf_path%/*}
 
-        if [[ $directory != $module_config_path ]]; then
+        if [[ $directory != $module_conf_path ]] && [[ ! -d $directory ]]; then
             # make required directories
             mkdir -p $directory
-            jpic_print_info "Created $directory for $module_config_path"
+            jpic_print_info "Created $directory for $module_conf_path"
         fi
 
-        touch $module_config_path
+        touch $module_conf_path
     fi
 
     for variable in ${!module_variables}; do
         local value="${!variable}"
     
-        grep -q "^${variable}=\"[^\"]*\"$" $module_config_path
+        grep -q "^${variable}=\"[^\"]*\"$" $module_conf_path
     
         if [[ $? -eq 0 ]]; then
-            sed -i -e "s@^${variable}=.*@${variable}=\"$value\"\$@" $module_config_path
+            sed -i -e "s@^${variable}=.*@${variable}=\"$value\"@" $module_conf_path
         else
-            echo "${variable}=\"${value}\"" >> $module_config_path
+            echo "${variable}=\"${value}\"" >> $module_conf_path
         fi
     done
 }
