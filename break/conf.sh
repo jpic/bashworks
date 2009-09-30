3fe162c19d40ca1b4fc7634a3c5f7fece8062f4d
jpic <jamespic@gmail.com>
2009-09-30 11:33:03 +0200

    This module has a couple of variables, no need for break_variables array

--- break/conf.sh
+++ break/conf.sh
@@ -6,30 +6,24 @@
 ##	@License	Apache
 #--------------------------
 
-unset break_variables
-break_variables+=("interval")
-break_variables+=("previous")
-# prefix variable names
-break_variables=("${break_variables[@]/#/break_}")
-
 #--------------------------
 ## Save your break configuration
 #--------------------------
 function break_conf_save() {
-    conf_save $break_conf_path $break_variables
+    conf_save $break_conf_path break_interval break_previous
 }
 
 #--------------------------
 ## Load your break configuration
 #--------------------------
 function break_conf_load() {
-    conf_save $break_conf_path
+    conf_load $break_conf_path
 }
 
 #--------------------------
 ## Interactive module configuration
 #--------------------------
 function break_conf_interactive() {
-    conf_interactive $break_variables
+    conf_interactive break_interval
     break_conf_save
 }
