5a46855c2805a8264de73455cbd7b893f5cc23bb
jpic <jamespic@gmail.com>
2009-10-04 17:53:31 +0200

    Fixed mtests_shunit2 module, tested with the 'hack' module

--- mtests/shunit2/source.sh
+++ mtests/shunit2/source.sh
@@ -1,14 +1,35 @@
-function mtest_shunit2() {
-    local module_name=$1
-    local module_path=$(module_get_path $module_name)
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#--------------------------
+## Wraps around shunit2 bash testing framework. Read shunit2/current/README for
+## information about working with shunit2.
+## <p>
+## Module tests which use shunit2 should be in a subdirectory of the module
+## named "shunit2".
+#--------------------------
 
-    if [[ ! -d $module_path/shunit2 ]]; then
+#--------------------------
+## Runs the shunit2 test suite of a given module.
+## @polite  Will try yourmodule_mtests_shunit2()
+#--------------------------
+function mtests_shunit2() {
+    local module_name="$1"
+    local module_path="$(module_get_path $module_name)"
+
+    # politeness snippet
+    local module_overload="${module_name}_mtests_shunit2"
+    if [[ $(declare -f $module_overload) ]]; then
+        if [[ ! ${FUNCNAME[*]} =~ $module_overload ]]; then
+            $module_overload
+            return $?
+        fi
+    fi
+
+    if [[ ! -d "$module_path/shunit2" ]]; then
         return 1
     fi
 
-    if [[ -f $module_path/shunit2.sh ]]; then
-        source $module_path/shunit2.sh
-    fi
+    source "$module_path"/shunit2/*.sh
 
-    source $module_path/shunit2/suite.sh
+    source "$(module_get_path mtests_shunit2)/current/src/shell/shunit2"
 }
