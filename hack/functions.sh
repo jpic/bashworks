bdfd0e89c153826d84a0356345924af623e7b31a
jpic <jamespic@gmail.com>
2009-10-04 17:48:46 +0200

    Simplified the output of hack_is_non_empty_array()

--- hack/functions.sh
+++ hack/functions.sh
@@ -1,11 +1,27 @@
 #--------------------------
-## Output something if a variable is a non empty array.
-## @param Variable name
+## Various bash hacks.
+#--------------------------
+
+#--------------------------
+## Check if a variable is a non empty array.
+## @stdout  "Yes" if the variable is a non empty array.
+## @param   Variable name
 #--------------------------
 function hack_is_non_empty_array() {
-    declare -a | grep $1=\'\(\\[;
+    declare -a | grep -q $1=\'\(\\[;
+
+    if [[ $? == 0 ]]; then
+        echo "Yes"
+    fi
 }
 
+#--------------------------
+## Adds each repository path to CDPATH environment variable.
+## <pre>
+## # change directory to somemodule, whatever is the current directory
+## cd somemodule
+## @variable    CDPATH, repo_path
+#--------------------------
 function hack_cdpath() {
     local repo_path=""
 
@@ -13,7 +29,7 @@ function hack_cdpath() {
         repo_path="${path%/*}"
 
         if [[ ! $CDPATH =~ "${repo_path}" ]]; then
-            CDPATH+=":${repo_path}"
+            export CDPATH+=":${repo_path}"
         fi
     done
 }
