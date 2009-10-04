a09ee15a3af34f89bffa1cf51f6120c54e3ae745
jpic <jamespic@gmail.com>
2009-10-04 17:51:39 +0200

    Fixed typos in mtests() and improved documentation

--- mtests/source.sh
+++ mtests/source.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #--------------------------
-##	@Synopsis	Break management module
-##	@Copyright	Copyright 2009, James Pic
-##	@License	Apache
-## This module wraps around xUnit frameworks:
+## This module wraps around xUnit frameworks through submodules:
 ## - bashunit: tested, working in module break,
 ## - shunit: untested, should work
-## - shunit2: untested
+## - shunit2: untested, recommanded
 #--------------------------
 
+#--------------------------
+## Runs all tests of a given module.
+## <p>
+## If the module has a "bashunit" sub directory then mtests_bashunit will be
+## called. Same goes for the other supported test frameworks: "shunit" and
+## "shunit2",
+## <p>
+## Shunit2 is recommanded.
+## @polite  Will try yourmodule_mtests()
+## @calls   mtests_bashunit, mtests_shunit, mtests_shunit2
+#--------------------------
 function mtests() {
     local module_name="$1"
-    local module_path="$(module_get_path $1)"
-    local module_test_function="${module_name}_test"
+    local module_path="$(module_get_path $module_name)"
+    local module_test_function="${module_name}_mtests"
 
     if [[ $(declare -f $module_test_function) ]]; then
         $module_test_function
