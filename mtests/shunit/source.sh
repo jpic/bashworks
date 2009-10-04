2f3b3bad7e9f8222fdb9173ebd07a163b5613012
jpic <jamespic@gmail.com>
2009-10-04 17:54:15 +0200

    Fixed documentation of mtests_shunit module and made mtests_shunit() polite

--- mtests/shunit/source.sh
+++ mtests/shunit/source.sh
@@ -1,19 +1,44 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#--------------------------
+## Wraps around shunit bash testing framework. Read shunit/current/README for
+## information about working with shunit.
+## <p>
+## Module tests which use shunit should be in a subdirectory of the module
+## named "shunit".
+#--------------------------
+
+#--------------------------
+## Sets SHUNIT_HOME environment variable required by shunit.
+#--------------------------
 function mtests_shunit_pre_source() {
     export SHUNIT_HOME="$(module_get_path mtests_shunit)/current"
 }
 
+#--------------------------
+## Loads shUnitPlus.
+#--------------------------
 function mtests_shunit_source() {
     source $(module_get_path mtests_shunit)/current/shUnitPlus >/dev/null 2>&1
 }
 
-function mtests_shunit_post_source() {
-    if [[ -z $SHUNIT_HOME ]]; then
-        SHUNIT_HOME=$(module_get_path mtests_shunit)/current
-    fi    
-}
-
+#--------------------------
+## Runs the test suites of a module.
+## @polite  Will try yourmodule_mtests_shunit()
+## @calls   shuStart
+#--------------------------
 function mtests_shunit() {
     local module_name=$1
+
+    local module_overload="${module_name}_mtests_shunit"
+
+    if [[ $(declare -f $module_overload) ]]; then
+        if [[ ! ${FUNCNAME[*]} =~ $module_overload ]]; then
+            $module_overload
+            return $?
+        fi
+    fi
+
     local module_path=$(module_get_path $module_name)
 
     if [[ ! -d $module_path/shunit ]]; then
