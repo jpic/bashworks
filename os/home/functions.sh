087be5f1197b521956993885fb1a22bf12a13911
jpic <jamespic@gmail.com>
2009-10-20 14:08:05 +0200

    Fugbix: variable rename was not finished

--- os/home/functions.sh
+++ os/home/functions.sh
@@ -3,7 +3,7 @@
 # OS/home management functions.
 
 # Symlinks any file from a subdirectory of $os_home_specifics_preix that matches
-# $OSTYPE will be symlinked to the appropriate directory of $os_home_prefix.
+# $OSTYPE will be symlinked to the appropriate directory of $os_home_real_prefix.
 function os_home_symlink() {
     local relative_target=""
     local target_file=""
@@ -17,7 +17,7 @@ function os_home_symlink() {
             relative_target="${dir#$home/}"
             
             # from bin to /home/jpic/bin
-            target_dir="$os_home_prefix/$relative_target"
+            target_dir="$os_home_real_prefix/$relative_target"
 
             if [[ ! -d "$target_dir" ]]; then
                 mlog info "Creating missing target directory: $target_dir"
@@ -31,7 +31,7 @@ function os_home_symlink() {
             relative_target="${file#$home/}"
             
             # from bin/foo to /home/jpic/bin/foo
-            target_file="$os_home_prefix/$relative_target"
+            target_file="$os_home_real_prefix/$relative_target"
             
             ln -sfn $file $target_file
         done
